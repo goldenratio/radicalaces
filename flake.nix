3a5d4a379a4fa32346fc06c6577c6b810ecd37ba
goldenratio <karthikeyanvj@gmail.com>
2025-08-09 14:14:29 +0300

    revert

--- flake.nix
+++ flake.nix
@@ -5,24 +5,32 @@
 
   outputs = { self, nixpkgs }:
     let
+      javaVersion = 8;
+
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f {
-          pkgs = import nixpkgs { inherit system; };
-        });
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
     in
     {
+      overlays.default =
+        final: prev:
+        let
+          jdk = prev."jdk${toString javaVersion}";
+        in
+        {
+          gradle = prev.gradle.override { java = jdk; };
+        };
+
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             gradle
-            jdk8
+            jdk
           ];
 
           shellHook =''
           echo "Hello Player! Welcome to Radical Aces."
-          echo "Using Java version: $(java -version 2>&1 | head -n1)"
-          echo "You can now run: ./run.sh"
           '';
         };
       });
